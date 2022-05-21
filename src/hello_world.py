84f347a99fa7170ec42475d58305834c076b43e5
Denny Tony Chakola <dtonychakola00@mylangara.ca>
2022-05-21 14:27:05 -0700

    Fourth Version - uploading hello_world.py in to src folder

---
+++ src/hello_world.py
@@ -0,0 +1,13 @@
+'''
+My first python code
+100375275
+Denny Tony Chakola
+'''
+
+print('Hello World !!!')
+name = ''
+name = input('Please enter your name: ')
+print('Welcome '+name+' to Computing to Data Analytics')
+print(f'Welcome {name} to Computing to Data Analytics')     # f is used to format the value of variable in the output
+print(f'Welcome {name.title()} to Computing to Data Analytics')     # this will format like "Denny Chakola"
+print(f'Welcome {name.upper()} to Computing to Data Analytics')     # this will display in capital letters
